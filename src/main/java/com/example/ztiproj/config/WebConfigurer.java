4c4b2f9be396d035daa63918c7e915c0691f851b
Mateusz Barnacki <barnackimateusz@gmail.com>
2022-09-10 19:08:41 +0200

    Add missing java doc

--- src/main/java/com/example/ztiproj/config/WebConfigurer.java
+++ src/main/java/com/example/ztiproj/config/WebConfigurer.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author Mateusz Barnacki
+ * @version 1.0
+ * @since 2022-09-10
+ */
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
 
