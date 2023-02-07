de0a1b4164d5ccff2742a39ba8566dc75f866895
mateuszbarnacki <barnackimateusz@gmail.com>
2023-02-07 23:05:02 +0100

    Remove unused labels

--- src/main/java/com/example/ztiproj/common/Labels.java
+++ src/main/java/com/example/ztiproj/common/Labels.java
@@ -6,34 +6,12 @@ package com.example.ztiproj.common;
  * @since 2022-08-09
  */
 public final class Labels {
-    public static final String USER_ENTITY = "user";
     public static final String MINESWEEPER_ENTITY = "minesweeper";
-    public static final String TETRIS_ENTITY = "tetris";
-    public static final String SNAKE_ENTITY = "snake";
-
-    public static final String USER_ENTITY_INDEX_NAME = "user_user_index";
     public static final String MINESWEEPER_ENTITY_INDEX_NAME = "minesweeper_user_index";
-    public static final String TETRIS_ENTITY_INDEX_NAME = "tetris_user_index";
-    public static final String SNAKE_ENTITY_INDEX_NAME = "snake_user_index";
-
     public static final String MINESWEEPER_USERNAME = "username";
     public static final String MINESWEEPER_TIME = "time";
-
-    public static final String SNAKE_USERNAME = "username";
-    public static final String SNAKE_SCORE = "score";
-
-    public static final String TETRIS_USERNAME = "username";
-    public static final String TETRIS_SCORE = "score";
-
-    public static final String USER_NAME = "name";
-    public static final String USER_USERNAME = "username";
-    public static final String USER_PASSWORD = "password";
-
     public static final String NULL_DTO_EXCEPTION_MESSAGE = "Null dto provided!";
-    public static final String INVALID_USER_DTO_EXCEPTION_MESSAGE = "Invalid user dto!";
-    public static final String EMPTY_USERNAME_EXCEPTION_MESSAGE = "Empty user name!";
-
     public static final String INVALID_MINESWEEPER_DTO_EXCEPTION_MESSAGE = "Invalid minesweeper dto!";
-    public static final String INVALID_SNAKE_DTO_EXCEPTION_MESSAGE = "Invalid snake dto!";
-    public static final String INVALID_TETRIS_DTO_EXCEPTION_MESSAGE = "Invalid tetris dto!";
+
+    private Labels() {}
 }
