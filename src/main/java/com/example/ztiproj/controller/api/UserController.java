08bc407fee61451e76817bfd52a40e4acee72564
mateuszbarnacki <barnackimateusz@gmail.com>
2023-02-07 23:01:37 +0100

    Remove user classes

--- src/main/java/com/example/ztiproj/controller/api/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.example.ztiproj.controller.api;
-
-/**
- * @author Mateusz Barnacki
- * @version 1.0
- * @since 2022-08-29
- */
-public interface UserController {
-}
--- src/main/java/com/example/ztiproj/controller/impl/UserControllerImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.ztiproj.controller.impl;
-
-/**
- * @author Mateusz Barnacki
- * @version 1.0
- * @since 2022-08-29
- */
-public class UserControllerImpl {
-}
